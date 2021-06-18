ca8941c2e442e5b05b75c01c13707a6836d8a9bd
sils-seminar <79352309+sils-seminar@users.noreply.github.com>
2021-06-18 22:42:33 +0900

    add config

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get 'hello/index' => 'hello#index'
+get "careers" => "careers#index"
+get "abroads" => "abroads#index"
+get "unis" => "unis#index"
+end
