3a5e4a24b62d8f7ae548648f7119b89adb3e7a70
sils-seminar <79352309+sils-seminar@users.noreply.github.com>
2021-06-18 23:27:23 +0900

    add

--- test/controllers/careers_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CareersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/unis_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class UnisControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
